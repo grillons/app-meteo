2e6c64ddec920d1f7e7417d83a72bf0157f110c4
Sandra <sandstudio@gmail.com>
2019-11-02 16:52:13 +0100

    Add react-navigation-stack - createStackNavigator() has been moved to react-navigation-stack

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,4 +1,5 @@
-import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from '../screens/HomeScreen';
 import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 import IntroScreen from '../screens/IntroScreen';
