fe097f2a11358fa37a1e5a1f9cb90c8984d03475
Sandra <sandra.grillon@gmail.com>
2019-11-02 22:40:38 +0100

    Add react-navigation-tabs

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,15 @@
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+//import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 import HomeScreen from '../screens/HomeScreen';
 import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 import IntroScreen from '../screens/IntroScreen';
 import IntroFormScreen from '../screens/IntroFormScreen';
 
-const AppStack = createStackNavigator({ Home: HomeScreen });
-const AuthStack = createStackNavigator({ SignIn: IntroFormScreen, Welcome: IntroScreen });
+//const AppStack = createStackNavigator({ Home: HomeScreen });
+//const AuthStack = createStackNavigator({ SignIn: IntroFormScreen, Welcome: IntroScreen });
+const AppStack = createBottomTabNavigator({ Home: HomeScreen, SignIn: IntroFormScreen });
+const AuthStack = createBottomTabNavigator({ SignIn: IntroFormScreen, Welcome: IntroScreen });
 
 export default createAppContainer(createSwitchNavigator(
   {
